f1eba1460db54faaf50ff415adbe505ffa692831
xchen <xchen601@gmail.com>
2024-04-28 14:10:30 -0400

    fix determine_move outliers, track soft hands

--- main.py
+++ main.py
@@ -26,14 +26,26 @@ class CardChecker:
         main_gray = cv2.cvtColor(main_image, cv2.COLOR_BGR2GRAY)
         template_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
 
-        # Apply template matching
-        res = cv2.matchTemplate(main_gray, template_gray, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.85  # Define a threshold for how good the match should be
-        loc = np.where(res >= threshold)
-        points = np.column_stack((loc[1], loc[0]))  # Switch x and y coordinates
+        # Define the scale range and the scaling factor
+        scale_range = np.linspace(0.6, 1.1, 7)  # Example range from 0.5x to 1.5x original size
+        match_results = []
+
+        for scale in scale_range:
+            # Resize the main image according to the current scale
+            resized_main = cv2.resize(main_gray, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+            res = cv2.matchTemplate(resized_main, template_gray, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.85
+            loc = np.where(res >= threshold)
+            if loc[0].size > 0:
+                for pt in zip(*loc[::-1]):
+                    match_results.append((pt[0] / scale, pt[1] / scale))
+
+        # Convert points to numpy array
+        points = np.array(match_results)
 
         if points.size == 0:
             return 0
+
         # Use DBSCAN to cluster points
         clustering = DBSCAN(eps=10, min_samples=1).fit(points)
         labels = clustering.labels_
@@ -47,11 +59,11 @@ class CardChecker:
             x_min, y_min = np.min(points_in_cluster, axis=0)
             x_max, y_max = np.max(points_in_cluster, axis=0)
             width, height = template_image.shape[1], template_image.shape[0]
-            top_left = (x_min, y_min)
-            bottom_right = (x_max + width, y_max + height)
+            top_left = (int(x_min), int(y_min))
+            bottom_right = (int(x_max + width), int(y_max + height))
             cv2.rectangle(main_image, top_left, bottom_right, (0, 255, 0), 2)
             count += 1
-        #
+
         # Convert color back to RGB for displaying in matplotlib
         main_image = cv2.cvtColor(main_image, cv2.COLOR_BGR2RGB)
 
@@ -59,8 +71,8 @@ class CardChecker:
         # plt.imshow(main_image)
         # plt.title('Matched Image')
         # plt.show()
-        #
-        print(f"{template_image_path} appears {count} times with {res.max()} confidence")
+
+        # print(f"{template_image_path} appears {count} times.")
         return count
 
     def get_cards(self, main_image_path, image_directory):
@@ -75,14 +87,14 @@ class CardChecker:
                 for i in range(count):
                     card_name = filename.split('.')[0]
                     cards.append(card_name)
-        print(cards)
+        # print(cards)
         return cards
 
 class BlackjackPlayer:
     def __init__(self):
         self.game_coordinates = [(2065, 727), (3671, 1628)]
         self.card_checker = CardChecker()
-        self.move_counts = 300
+        self.move_counts = 400
 
     def get_bj_image(self, upper_left_x, upper_left_y, lower_right_x, lower_right_y):
         width = lower_right_x - upper_left_x
@@ -117,16 +129,18 @@ class BlackjackPlayer:
                               self.game_coordinates[1][0], self.game_coordinates[1][1])
 
             self.make_move()
-            time.sleep(3)
     def make_move(self):
         # check if respin or no action
         if self.is_round_end():
             self.click_image('actions/respin.png')
+            print('Respin')
             return
         if self.is_insurance():
             self.click_image('actions/no.png')
+            print('No')
             return
-
+        self.get_bj_image(self.game_coordinates[0][0], self.game_coordinates[0][1],
+                          self.game_coordinates[1][0], self.game_coordinates[1][1])
         dealer_hand = self.card_checker.get_cards('dealer.png', 'cards')
         player_hand = self.card_checker.get_cards('player.png', 'cards')
         print('dealer hand:', dealer_hand)
@@ -134,7 +148,8 @@ class BlackjackPlayer:
         if len(dealer_hand) == 0 or len(player_hand) < 2:
             print('error getting cards')
             return
-
+        if len(dealer_hand) != 1:
+            return
 
         move = self.determine_move(player_hand, dealer_hand)
         print(move)
@@ -148,29 +163,41 @@ class BlackjackPlayer:
         if move == 'Split':
             self.click_image('actions/split.png')
             print('split triggered')
+        time.sleep(4)
+    def get_region(self):
+        width = self.game_coordinates[1][0] - self.game_coordinates[0][0]
+        height = self.game_coordinates[1][1] - self.game_coordinates[0][1]
+        region = (self.game_coordinates[0][0], self.game_coordinates[0][1], width, height)
+        return region
+
     def click_image(self, image_path):
+        # limit search region so it faster
+        region = self.get_region()
+
         curr_location = pyautogui.position()
-        location = pyautogui.locateCenterOnScreen(image_path, confidence=0.8)
+        location = pyautogui.locateCenterOnScreen(image_path, region=region, confidence=0.8)
         pyautogui.click(location)
         pyautogui.moveTo(curr_location[0], curr_location[1])
 
-    def is_round_end(self):
-        location = pyautogui.locateCenterOnScreen('actions/respin.png', confidence=0.8)
 
+    def is_round_end(self):
+        region = self.get_region()
+        location = pyautogui.locateCenterOnScreen('actions/respin.png', region=region, confidence=0.8)
         if location:
             return True
         return False
 
     def is_insurance(self):
-        location = pyautogui.locateCenterOnScreen('actions/no.png', confidence=0.8)
+        region = self.get_region()
+        location = pyautogui.locateCenterOnScreen('actions/no.png', region=region, confidence=0.8)
 
         if location:
             return True
         return False
 
     def determine_move(self, player_hand, dealer_hand):
-        soft = False
         def calculate_total(hand):
+            soft = False
             total = 0
             ace_count = 0
             for card in hand:
@@ -187,38 +214,39 @@ class BlackjackPlayer:
             while total > 21 and ace_count:
                 total -= 10
                 ace_count -= 1
-            return total
+            return total, soft
 
         # Calculate totals
-        player_total = calculate_total(player_hand)
-        dealer_total = calculate_total(dealer_hand)
+        player_total, soft = calculate_total(player_hand)
+        dealer_total, _ = calculate_total(dealer_hand)
 
+        print("soft:", soft)
         if player_total <= 8:
             return 'Hit'
         # conditions for double
-        if (len(player_hand) == 2) and (((3 <= dealer_total <= 6) and player_total == 9)
+        if ((len(player_hand) == 2) and
+                (((3 <= dealer_total <= 6) and player_total == 9)
                 or ((2 <= dealer_total <= 9) and player_total == 10)
                 or ((2 <= dealer_total <= 10) and player_total == 11)
                 or ((5 <= dealer_total <= 6) and player_total in (13, 14) and soft)
                 or ((4 <= dealer_total <= 6) and player_total in (15, 16) and soft)
-                or ((3 <= dealer_total <= 6) and player_total in (17, 18) and soft)):
+                or ((3 <= dealer_total <= 6) and player_total in (17, 18) and soft))):
             return 'Double'
         # conditions for splitting
         if (len(player_hand) == 2) and (player_hand[0] == player_hand[1]):
             card = player_hand[0]
-            if (card in ('a', '8')
-                    or (card in (2, 3) and dealer_total <= 7)
-                    or (card == 6 and dealer_total <= 6)
-                    or (card == 7 and dealer_total <= 7)
-                    or (card == 9 and (dealer_total <= 6 or dealer_total in (8, 9)))):
+            if (card in ['a', '8']
+                    or (card in ("2", "3") and dealer_total <= 7)
+                    or (card == "6" and dealer_total <= 6)
+                    or (card == "7" and dealer_total <= 7)
+                    or (card == "9" and (dealer_total <= 6 or dealer_total in (8, 9)))):
                 return 'Split'
         # conditions for Hit
         if (player_total <= 11
                 or (player_total == 12 and dealer_total in (2, 3))
                 or (player_total <= 16 and dealer_total >= 7)
                 or (player_total <= 17 and soft)
-                or (player_total == 18 and soft and dealer_total >= 9)
-                or (player_total <= 14)):
+                or (player_total == 18 and soft and dealer_total >= 9)):
             return 'Hit'
 
         return 'Stand'
